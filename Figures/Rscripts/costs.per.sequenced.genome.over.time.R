c0d6629dfd5e725c8e76b8c4f3b9bd2d283842a2
Christian Roy <christiankroy@gmail.com>
2014-05-05 16:54:59 -0400

    Incorp lab comments. Into last pas be4 first sub

--- Figures/Rscripts/costs.per.sequenced.genome.over.time.R
+++ Figures/Rscripts/costs.per.sequenced.genome.over.time.R
@@ -9,27 +9,32 @@
 # They ask to be cited as
 # Wetterstrand KA. DNA Sequencing Costs: Data from the NHGRI Genome Sequencing Program (GSP) Available at: www.genome.gov/sequencingcosts. Accessed [date of access].
 
-
-costs <- read.csv("Figures/01-Introduction/sequencing_cost_apr13.csv",sep=",",header=T)
-costs$Date <- as.POSIXct(as.character(costs$Date), format = "%m/%d/%Y")
+library(sigalphTools)
+costs <- read.csv("Figures/SupportingData/sequencing_cost_20140131_withMoore.csv",sep=",",header=T)
+costs$Date <- as.POSIXct(as.character(costs$Date), format = "%Y-%m-%d")
 head(costs)
 tail(costs)
-library(ggplot2)
-pdf(file="Figures/01-Introduction/Sequencing.costs.over.time.pdf",width=7,height=3,paper="letter")
-ggplot(costs,aes(x=Date, y= perGenome)) +
-  ggtitle("Cost per sequenced genome over time") +
+str(costs)
+pdf(file="Figures/Intro/Sequencing.costs.over.time.pdf",width=7,height=3,paper="letter")
+p <- 
+  ggplot(costs,aes(x=Date, y= perGenome)) +
+ # ggtitle("Cost per sequenced genome over time") +
   xlab("Date") +
   ylab("Cost in US Dollars") +
-  geom_line(stat="identity") +
+  geom_line(stat="identity") +    
   scale_y_log10(breaks=c(     
-                            #  10,
+                              #1,
                              # 100,
-                            #  1000,
+                              1000,
                               10000,
                             #  100000,
                               1000000,
                             #  10000000,
                               100000000),
-                labels=c("10K","1 Million","100 Million")) +  
+                labels=c("1K","10K","1 Million","100 Million"),
+                limits=c(1000,100000000)) +  
   theme_bw (base_size = 16) 
+  p + geom_line(aes(x=Date,y=mooreCost))
 graphics.off()
+
+
--- test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash  
-htlatex RoyC_Umass_Thesis.tex "LaTeXConfig/configureHTMLImageOutput.cfg" "html,charset=utf-8" "--interaction=nonstopmode" "-dHTML" 
